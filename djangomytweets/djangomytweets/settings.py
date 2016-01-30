c01b64fc7ded4875f01a09030aa7700684bed37f
Andrew Trofimchuk <zipppt@gmail.com>
2016-01-30 02:22:30 +0200

    project DJ

--- djangomytweets/djangomytweets/settings.py
+++ djangomytweets/djangomytweets/settings.py
@@ -74,6 +74,7 @@ WSGI_APPLICATION = 'djangomytweets.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
